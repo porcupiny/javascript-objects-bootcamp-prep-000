acb3a671740de3d444a24b0ffb99f9f1a11f36ec
Liane Chan <liane.chan@gmail.com>
2018-05-28 18:10:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,10 @@
 var playlist = new Object({
-  artistName: songTitle,
   'New Order': 'Ceremony',
   'Echo & the Bunnymen': 'A Promise'
 })
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return playlist.assign({}, artistName, songTitle)
+  return playlist.assign({}, {artistName: songTitle})
 }
 
 function removeFromPlaylist(playlist, artistName) {
