88dc79486f0cf77d86bd265070c7ecc6b4251eb0
Liane Chan <liane.chan@gmail.com>
2018-05-28 18:13:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,7 @@ var playlist = new Object({
 
 function updatePlaylist(playlist, artistName, songTitle) {
  playlist[artistName]=songTitle
+ return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
